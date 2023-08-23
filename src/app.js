9d765e68e7f4ed5da71334f59539cd5b985543b9
James Chartrand <jc.chartrand@gmail.com>
2023-08-23 16:57:30 -0400

    return json from errors

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ import allocateStatus from './allocateStatus.js'
 export async function build(opts = {}) {
 
     await initializeStatusManager()
-    
+
     var app = express();
 
     app.use(logger('dev'));
@@ -21,7 +21,7 @@ export async function build(opts = {}) {
     });
 
     // allocate status
-    app.post("/credentials/status/allocate", 
+    app.post("/credentials/status/allocate",
         async (req, res) => {
             try {
                 const vc = req.body;
@@ -29,34 +29,35 @@ export async function build(opts = {}) {
                 return res.json(vcWithStatus)
             } catch (error) {
                 console.log(error);
-                return res.status(403).json(error);
+                return res.status(403).json({code: 403, error, message: "Error when allocating status position."});
             }
         })
 
     // the body will look like:  {credentialId: '23kdr', credentialStatus: [{type: 'StatusList2021Credential', status: 'revoked'}]}
-    app.post("/credentials/status", 
+    app.post("/credentials/status",
         async (req, res) => {
             try {
-                 const instanceId = req.params.instanceId 
-            
-                if (!req.body || !Object.keys(req.body).length ) return res.status(400).send('No update request was provided in the body')
-                
-                const {credentialId, credentialStatus} = req.body;
+                const instanceId = req.params.instanceId
+
+                if (!req.body || !Object.keys(req.body).length) return res.status(400).send({code: 400, message: 'No update request was provided in the body.'})
+
+                const { credentialId, credentialStatus } = req.body;
                 const status = credentialStatus[0].status
                 const statusType = credentialStatus[0].type
-                if (statusType === 'StatusList2021Credential') {
-                    const statusResponse = await revoke(credentialId, status)
-                    return res.status(statusResponse.code).send(statusResponse.message)
-                } else {
-                    return res.status(400).send('StatusList2021Credential is the only supported revocation mechanism.')
-                }
+
+                const statusResponse = (statusType === 'StatusList2021Credential') ?
+                    await revoke(credentialId, status)
+                    :
+                    { code: 400, message: 'StatusList2021Credential is the only supported revocation mechanism.' }
+
+                return res.status(statusResponse.code).send(statusResponse)
             } catch (error) {
                 console.log(error);
-                return res.status(500).json(error);
+                return res.status(500).json({code: 50, error, message: "Error updating credential status."});
             }
         })
 
-  
+
 
     return app;
 
